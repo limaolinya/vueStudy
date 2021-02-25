092990c3bddf8eb74c8790a9a3986b8c9066f6dc
li <2681291959@qq.com>
2021-02-25 17:50:43 +0800

    上传图片组件，还有错误

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+import upload from "@/components/upload";
+Vue.component("upload", upload);
+
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
+  el: "#app",
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
+
+
+
+
+
